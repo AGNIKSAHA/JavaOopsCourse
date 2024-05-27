ea9f73ba6fb895a3d329b0e87424343f5da03bab
agniksaha <agniksaha9953@gmail.com>
2024-05-28 01:41:46 +0530

    javaFirst

--- src/mySecondPackage/getterSetterTest.java
+++ src/mySecondPackage/getterSetterTest.java
@@ -6,6 +6,7 @@ public class getterSetterTest {
     public static void main(String[] args) {
         Car car=new Car("blue","ml123",12);
         car.setColor("red");
+        car.setModel("m29");
         System.out.println(car.getColor());
         System.out.println(car.getModel());
 
